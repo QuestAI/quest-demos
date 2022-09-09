1344469c783fb6a3a96b0d87b673863d5a7a6fbc
Thiago Costa <thiagocosta@Thiagos-MacBook-Pro.local>
2022-09-09 16:21:16 -0300

    Initial setup

---
+++ src/components/MyDesignSystemDarkTheme.ts
@@ -0,0 +1,31 @@
+
+import { createTheme } from "@mui/material/styles";
+
+   
+const globalTheme: any = createTheme({
+  palette: {
+   "mode": "dark"
+ },
+  typography: {},
+  customShadows: {
+   "shadows": {
+     "boxShadow": "0px 3px 12px rgba(0, 0, 0, 0.12)"
+   },
+   "bg blur": {
+     "backdropFilter": "blur(24px)",
+     "WebkitBackdropFilter": "blur(24px)"
+   }
+ },
+} as any);
+
+const questTheme = createTheme({
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      }
+    },
+  }
+}, globalTheme);
+
+export default questTheme;
